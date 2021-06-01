885f6279e097e9d9e573c2d89dcdc59e7a7c5a5d
Bianca Elaine Tuno <biancatuno@gmail.com>
2021-06-01 23:00:59 +0800

    Confirm button can now save info

--- Contact Tracing App/Form1.Designer.cs
+++ Contact Tracing App/Form1.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Contact_Tracing_App
 {
     partial class contact_tracing_app
@@ -30,19 +32,24 @@ namespace Contact_Tracing_App
         private void InitializeComponent()
         {
             this.firstname = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txt_firstname = new System.Windows.Forms.TextBox();
+            this.txt_middlename = new System.Windows.Forms.TextBox();
             this.middlename = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txt_lastname = new System.Windows.Forms.TextBox();
             this.lastname = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txt_age = new System.Windows.Forms.TextBox();
             this.age = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txt_address = new System.Windows.Forms.TextBox();
             this.address = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.txt_contact = new System.Windows.Forms.TextBox();
             this.contact_num = new System.Windows.Forms.Label();
             this.add_details = new System.Windows.Forms.Button();
             this.confirm = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // firstname
@@ -53,21 +60,20 @@ namespace Contact_Tracing_App
             this.firstname.Size = new System.Drawing.Size(80, 20);
             this.firstname.TabIndex = 0;
             this.firstname.Text = "First Name";
-            this.firstname.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // txt_firstname
             // 
-            this.textBox1.Location = new System.Drawing.Point(207, 42);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(323, 27);
-            this.textBox1.TabIndex = 1;
+            this.txt_firstname.Location = new System.Drawing.Point(222, 42);
+            this.txt_firstname.Name = "txt_firstname";
+            this.txt_firstname.Size = new System.Drawing.Size(323, 27);
+            this.txt_firstname.TabIndex = 1;
             // 
-            // textBox2
+            // txt_middlename
             // 
-            this.textBox2.Location = new System.Drawing.Point(207, 96);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(323, 27);
-            this.textBox2.TabIndex = 3;
+            this.txt_middlename.Location = new System.Drawing.Point(222, 96);
+            this.txt_middlename.Name = "txt_middlename";
+            this.txt_middlename.Size = new System.Drawing.Size(323, 27);
+            this.txt_middlename.TabIndex = 3;
             // 
             // middlename
             // 
@@ -78,12 +84,12 @@ namespace Contact_Tracing_App
             this.middlename.TabIndex = 2;
             this.middlename.Text = "Middle Name ";
             // 
-            // textBox3
+            // txt_lastname
             // 
-            this.textBox3.Location = new System.Drawing.Point(207, 151);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(323, 27);
-            this.textBox3.TabIndex = 5;
+            this.txt_lastname.Location = new System.Drawing.Point(222, 151);
+            this.txt_lastname.Name = "txt_lastname";
+            this.txt_lastname.Size = new System.Drawing.Size(323, 27);
+            this.txt_lastname.TabIndex = 5;
             // 
             // lastname
             // 
@@ -94,12 +100,12 @@ namespace Contact_Tracing_App
             this.lastname.TabIndex = 4;
             this.lastname.Text = "Last Name";
             // 
-            // textBox4
+            // txt_age
             // 
-            this.textBox4.Location = new System.Drawing.Point(207, 203);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(323, 27);
-            this.textBox4.TabIndex = 7;
+            this.txt_age.Location = new System.Drawing.Point(222, 203);
+            this.txt_age.Name = "txt_age";
+            this.txt_age.Size = new System.Drawing.Size(323, 27);
+            this.txt_age.TabIndex = 7;
             // 
             // age
             // 
@@ -110,12 +116,13 @@ namespace Contact_Tracing_App
             this.age.TabIndex = 6;
             this.age.Text = "Age";
             // 
-            // textBox5
+            // txt_address
             // 
-            this.textBox5.Location = new System.Drawing.Point(207, 255);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(323, 27);
-            this.textBox5.TabIndex = 9;
+            this.txt_address.Location = new System.Drawing.Point(222, 248);
+            this.txt_address.Multiline = true;
+            this.txt_address.Name = "txt_address";
+            this.txt_address.Size = new System.Drawing.Size(323, 61);
+            this.txt_address.TabIndex = 9;
             // 
             // address
             // 
@@ -126,17 +133,17 @@ namespace Contact_Tracing_App
             this.address.TabIndex = 8;
             this.address.Text = "Address";
             // 
-            // textBox6
+            // txt_contact
             // 
-            this.textBox6.Location = new System.Drawing.Point(207, 310);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(323, 27);
-            this.textBox6.TabIndex = 11;
+            this.txt_contact.Location = new System.Drawing.Point(222, 328);
+            this.txt_contact.Name = "txt_contact";
+            this.txt_contact.Size = new System.Drawing.Size(323, 27);
+            this.txt_contact.TabIndex = 11;
             // 
             // contact_num
             // 
             this.contact_num.AutoSize = true;
-            this.contact_num.Location = new System.Drawing.Point(79, 317);
+            this.contact_num.Location = new System.Drawing.Point(79, 335);
             this.contact_num.Name = "contact_num";
             this.contact_num.Size = new System.Drawing.Size(118, 20);
             this.contact_num.TabIndex = 10;
@@ -144,7 +151,7 @@ namespace Contact_Tracing_App
             // 
             // add_details
             // 
-            this.add_details.Location = new System.Drawing.Point(355, 382);
+            this.add_details.Location = new System.Drawing.Point(370, 382);
             this.add_details.Name = "add_details";
             this.add_details.Size = new System.Drawing.Size(175, 29);
             this.add_details.TabIndex = 12;
@@ -153,31 +160,92 @@ namespace Contact_Tracing_App
             // 
             // confirm
             // 
-            this.confirm.Location = new System.Drawing.Point(556, 382);
+            this.confirm.Location = new System.Drawing.Point(567, 382);
             this.confirm.Name = "confirm";
             this.confirm.Size = new System.Drawing.Size(94, 29);
             this.confirm.TabIndex = 13;
             this.confirm.Text = "Confirm";
             this.confirm.UseVisualStyleBackColor = true;
+            this.confirm.Click += new System.EventHandler(this.confirm_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(551, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 22);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(551, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 22);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(551, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 22);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(551, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 22);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(551, 328);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 22);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "*";
             // 
             // contact_tracing_app
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(682, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.confirm);
             this.Controls.Add(this.add_details);
-            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.txt_contact);
             this.Controls.Add(this.contact_num);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.txt_address);
             this.Controls.Add(this.address);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txt_age);
             this.Controls.Add(this.age);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txt_lastname);
             this.Controls.Add(this.lastname);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txt_middlename);
             this.Controls.Add(this.middlename);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_firstname);
             this.Controls.Add(this.firstname);
             this.MaximizeBox = false;
             this.Name = "contact_tracing_app";
@@ -191,19 +259,25 @@ namespace Contact_Tracing_App
         #endregion
 
         private System.Windows.Forms.Label firstname;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txt_firstname;
+        private System.Windows.Forms.TextBox txt_middlename;
         private System.Windows.Forms.Label middlename;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txt_lastname;
         private System.Windows.Forms.Label lastname;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txt_age;
         private System.Windows.Forms.Label age;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txt_address;
         private System.Windows.Forms.Label address;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txt_contact;
         private System.Windows.Forms.Label contact_num;
         private System.Windows.Forms.Button add_details;
         private System.Windows.Forms.Button confirm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private readonly EventHandler label1_Click;
     }
 }
 
--- Contact Tracing App/Form1.cs
+++ Contact Tracing App/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Contact_Tracing_App
 {
@@ -17,8 +18,18 @@ namespace Contact_Tracing_App
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void confirm_Click(object sender, EventArgs e)
         {
+            StreamWriter A = new StreamWriter(Application.StartupPath + "\\form\\" + "Details.txt");
+
+            A.WriteLine(firstname.Text + ":" + " " + txt_firstname.Text);
+            A.WriteLine(middlename.Text + ":" + " " + txt_middlename.Text);
+            A.WriteLine(lastname.Text + ":" + " " + txt_lastname.Text);
+            A.WriteLine(age.Text + ":" + " " + txt_age.Text);
+            A.WriteLine(address.Text + ":" + " " + txt_address.Text);
+            A.WriteLine(contact_num.Text + ":" + " " + txt_contact.Text);
+
+            A.Close();
 
         }
     }
